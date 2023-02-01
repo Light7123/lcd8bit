e074140576077d49d64e685ad6c6d4fa36b38602
Light7123 <108993505+Light7123@users.noreply.github.com>
2023-02-01 16:33:10 +0400

    123

--- Core/Src/lcd_menu.cpp
+++ Core/Src/lcd_menu.cpp
@@ -11,31 +11,107 @@
 
 Row* selectedMenuItem;
 
-Row* start_menu			= new Row({"root"});
-Row* stat_menu			= new Row({"root"});
-Row* diag_menu			= new Row({"root"});
-Row* setting_menu			= new Row({"root"});
-Row* peref_menu =  new Row({"root"});
-int str1[11]={145, 226, 160, 226, 168, 225, 226, 168, 170, 160, 300};
-int str2[12]={132, 168, 160, 163, 173, 174, 225, 226, 168, 170, 160, 300};
-int str3[14]={141, 160, 225, 226, 224, 174, 169, 170, 168, 300};
-int str4[14]={143, 224, 174, 164, 160, 166, 160, 300};
-int str5[14]={129, 165, 167, 173, 160, 171, 168, 231, 173, 235, 165, 300};
-int str6[14]={141, 160, 171, 168, 231, 173, 235, 165, 300};
-int str7[14]={145, 161, 224, 174, 225, 32, 145, 226, 160, 226, 300};
-int str8[14]={132, 160, 226, 160, 32, 167, 160, 175, 224, 160, 162, 170, 168, 300};
-int str9[14]={146, 165, 225, 226, 174, 162, 160, 239, 32, 300};
-int str10[14]={143, 165, 224, 165, 228, 165, 224, 168, 239, 300};
-int str11[14]={129, 165, 167, 173, 160, 171, 168, 231, 173, 235, 165, 300};
-int str12[14]={141, 160, 171, 168, 231, 173, 235, 165, 300};
-int str13[14]={146, 165, 225, 226, 32, 170, 173, 174, 175, 174, 170, 300};
-int str14[14]={140, 174, 164, 165, 172, 300};
-int str15[14]={143, 171, 160, 226, 235, 32, 224, 160, 225, 232, 168, 224, 46, 300};
-int str16[14]={138, 160, 171, 168, 161, 224, 174, 162, 170, 160, 300};
-int str17[14]={158, 225, 226, 168, 224, 174, 162, 170, 160, 300};
-int str18[14]={143, 224, 160, 169, 225, 45, 171, 168, 225, 226, 300};
-int str19[14]={141, 160, 171, 168, 231, 173, 235, 165, 300};
-int str20[14]={143, 171, 160, 226, 235, 32, 224, 160, 225, 232, 168, 224, 46, 300};
+Row* start_menu = new Row({"root"});
+Row* stat_menu = new Row({"root"});
+Row* diag_menu = new Row({"root"});
+Row* setting_menu = new Row({"root"});
+Row* peref_menu = new Row({"root"});
+Row* prodaja_stat = new Row({"root"});
+Row* beznal_stat = new Row({"root"});
+Row* nal_stat = new Row({"root"});
+Row* sbros_stat = new Row({"root"});
+Row* data_stat = new Row({"root"});
+Row* beznal_peref = new Row({"root"});
+Row* oplata_qr = new Row({"root"});
+Row* terminal = new Row({"root"});
+Row* loyal = new Row({"root"});
+Row* nal_peref = new Row({"root"});
+Row* kupurpriem = new Row({"root"});
+Row* test_button = new Row({"root"});
+Row* modem_peref = new Row({"root"});
+Row* rashirenie_peref = new Row({"root"});
+Row* bak_peref = new Row({"root"});
+Row* kalibrovka = new Row({"root"});
+Row* ustirowka = new Row({"root"});
+Row* price = new Row({"root"});
+Row* price_obem = new Row({"root"});
+Row* price_cena = new Row({"root"});
+Row* setting_nal = new Row({"root"});
+Row* setting_kupur = new Row({"root"});
+Row* setting_rashir = new Row({"root"});
+Row* bak_setting = new Row({"root"});
+Row* zamena = new Row({"root"});
+Row* data_zamen = new Row({"root"});
+Row* data_zapr = new Row({"root"});
+int str[67][14] = {
+{145, 226, 160, 226, 168, 225, 226, 168, 170, 160, 300},
+{132, 168, 160, 163, 173, 174, 225, 226, 168, 170, 160, 300},
+{141, 160, 225, 226, 224, 174, 169, 170, 168, 300},
+{143, 224, 174, 164, 160, 166, 160, 300},
+{129, 165, 167, 173, 160, 171, 168, 231, 173, 235, 165, 300},
+{141, 160, 171, 168, 231, 173, 235, 165, 300},
+{145, 161, 224, 174, 225, 32, 145, 226, 160, 226, 300},
+{132, 160, 226, 160, 32, 167, 160, 175, 224, 160, 162, 170, 168, 300},
+{146, 165, 225, 226, 174, 162, 160, 239, 32, 300},
+{143, 165, 224, 168, 228, 165, 224, 168, 239, 300},
+{129, 165, 167, 173, 160, 171, 168, 231, 173, 235, 165, 300},
+{141, 160, 171, 168, 231, 173, 235, 165, 300},
+{146, 165, 225, 226, 32, 170, 173, 174, 175, 174, 170, 300},
+{140, 174, 164, 165, 172, 300},
+{143, 171, 160, 226, 235, 32, 224, 160, 225, 232, 168, 224, 46, 300},
+{138, 160, 171, 168, 161, 224, 174, 162, 170, 160, 300},
+{158, 225, 226, 168, 224, 174, 162, 170, 160, 300},
+{143, 224, 160, 169, 225, 45, 171, 168, 225, 226, 300},
+{141, 160, 171, 168, 231, 173, 235, 165, 300},
+{143, 171, 160, 226, 235, 32, 224, 160, 225, 232, 168, 224, 46, 300},
+{130, 174, 164, 160, 300},
+{145, 129, 143, 300},
+{146, 165, 224, 172, 168, 173, 160, 171, 300},
+{139, 174, 239, 171, 236, 173, 174, 225, 226, 236, 300},
+{138, 227, 175, 238, 224, 174, 175, 224, 168, 165, 172, 300},
+{145, 161, 224, 174, 225, 168, 226, 236, 300},
+{145, 161, 224, 174, 225, 168, 226, 236, 300},
+{142, 175, 171, 160, 226, 160, 32, 175, 174, 32, 81, 82, 300},
+{146, 165, 224, 172, 168, 173, 160, 171, 300},
+{139, 174, 239, 171, 236, 173, 174, 225, 226, 236, 300},
+{145, 226, 160, 226, 227, 225, 300},
+{145, 226, 160, 226, 227, 225, 300},
+{140, 174, 164, 165, 171, 236, 300},
+{145, 226, 160, 226, 227, 225, 300},
+{138, 227, 175, 238, 224, 174, 175, 224, 168, 165, 172, 300},
+{145, 226, 160, 226, 227, 225, 300},
+{140, 174, 164, 165, 171, 236, 300},
+{32, 146, 165, 225, 226, 32, 170, 173, 174, 175, 174, 170, 300},
+{73, 77, 69, 73, 300},
+{73, 67, 67, 73, 68, 300},
+{67, 83, 81, 300},
+{67, 83, 80, 78, 300},
+{142, 231, 168, 225, 226, 170, 160, 47, 161, 160, 170, 300},
+{145, 226, 160, 226, 227, 225, 300},
+{144, 160, 225, 229, 174, 164, 172, 165, 224, 300},
+{142, 161, 234, 165, 172, 32, 49, 300},
+{142, 161, 234, 165, 172, 32, 50, 300},
+{142, 161, 234, 165, 172, 32, 51, 300},
+{142, 161, 234, 165, 172, 300},
+{150, 165, 173, 160, 300},
+{142, 161, 234, 165, 172, 32, 49, 300},
+{142, 161, 234, 165, 172, 32, 50, 300},
+{142, 161, 234, 165, 172, 32, 51, 300},
+{142, 161, 234, 165, 172, 32, 49, 300},
+{142, 161, 234, 165, 172, 32, 50, 300},
+{142, 161, 234, 165, 172, 32, 51, 300},
+{138, 227, 175, 238, 224, 174, 175, 224, 168, 165, 172, 300},
+{136, 173, 170, 32, 225, 173, 239, 226, 32, 225, 226, 165, 170, 300},
+{142, 231, 168, 225, 226, 170, 160, 47, 161, 160, 170, 300},
+{144, 165, 166, 168, 172, 32, 224, 160, 161, 174, 226, 235, 300},
+{135, 160, 175, 224, 160, 162, 47, 167, 160, 172, 165, 173, 160, 300},
+{132, 160, 226, 160, 32, 167, 160, 175, 224, 160, 162, 170, 168, 300},
+{130, 224, 227, 231, 173, 227, 238, 300},
+{128, 162, 226, 174, 172, 160, 226, 168, 231, 165, 225, 170, 168, 300},
+{132, 160, 226, 160, 32, 167, 160, 172, 165, 173, 235, 300},
+{130, 224, 227, 231, 173, 227, 238, 300},
+{128, 162, 226, 174, 172, 160, 226, 168, 231, 165, 225, 170, 168, 300}
+};
 void menu_init()
 {
 	//������������������������������������������������������ ������������������������
@@ -48,29 +124,85 @@ void menu_init()
 
 
 	//������������������������������������������������������������
-	stat_menu->addRow({"prodaja",4})->parent=start_menu;
-	stat_menu->addRow({"beznal",5})->parent=start_menu;
-	stat_menu->addRow({"nal",6})->parent=start_menu;
-	stat_menu->addRow({"sbros_stat",7})->parent=start_menu;
-	stat_menu->addRow({"data zameni",8})->parent=start_menu;
+	stat_menu->parent=start_menu;
+	stat_menu->addRow({"prodaja",4})->children.push_back(prodaja_stat);
+		prodaja_stat->addRow({"Voda",21})->parent=stat_menu;
+	stat_menu->addRow({"beznal",5})->children.push_back(beznal_stat);
+		beznal_stat->addRow({"SBP",22})->parent=stat_menu;
+		beznal_stat->addRow({"terminal",23})->parent=stat_menu;
+		beznal_stat->addRow({"loyal",24})->parent=stat_menu;
+	stat_menu->addRow({"nal",6})->children.push_back(nal_stat);
+		nal_stat->addRow({"купюроприемник",25})->parent=stat_menu;
+	stat_menu->addRow({"sbros_stat",7})->children.push_back(sbros_stat);
+		sbros_stat->addRow({"sbros",26})->parent=stat_menu;
+	stat_menu->addRow({"data zameni",8})->children.push_back(data_stat);
+		data_stat->addRow({"sbros",27})->parent=stat_menu;
 
 	//������������������������������������������������������������������
+		diag_menu->parent=start_menu;
 	diag_menu->addRow({"test pokupka",9})->parent=start_menu;
 	diag_menu->addRow({"perefiriya",10})->children.push_back(peref_menu);
-
-	peref_menu->addRow({"Безнал",11})->parent=diag_menu;
-	peref_menu->addRow({"Наличные",12})->parent=diag_menu;
-	peref_menu->addRow({"Тест кнопок",13})->parent=diag_menu;
-	peref_menu->addRow({"Модем",14})->parent=diag_menu;
-	peref_menu->addRow({"Платы расширения",15})->parent=diag_menu;
+	peref_menu->parent=diag_menu;
+	peref_menu->addRow({"Безнал",11})->children.push_back(beznal_peref);
+		beznal_peref->parent=peref_menu;
+		beznal_peref->addRow({"oplata qr",28})->children.push_back(oplata_qr);
+			oplata_qr->addRow({"status",31})->parent=beznal_peref;
+		beznal_peref->addRow({"terminal",29})->children.push_back(terminal);
+			terminal->addRow({"status",32})->parent=beznal_peref;
+			terminal->addRow({"model",33})->parent=beznal_peref;
+		beznal_peref->addRow({"loyal",30})->children.push_back(loyal);
+			loyal->addRow({"status",34})->parent=beznal_peref;
+	peref_menu->addRow({"Наличные",12})->children.push_back(nal_peref);
+		nal_peref->parent=peref_menu;
+		nal_peref->addRow({"Купюроприемник",35})->children.push_back(kupurpriem);
+			kupurpriem->addRow({"status",36})->parent=nal_peref;
+			kupurpriem->addRow({"model",37})->parent=nal_peref;
+	peref_menu->addRow({"Тест кнопок",13})->children.push_back(test_button);
+		test_button->addRow({"тест",38})->parent=peref_menu;
+	peref_menu->addRow({"Модем",14})->children.push_back(modem_peref);
+		modem_peref->addRow({"imei",39})->parent=peref_menu;
+		modem_peref->addRow({"iccid",40})->parent=peref_menu;
+		modem_peref->addRow({"csq",41})->parent=peref_menu;
+		modem_peref->addRow({"cspn",42})->parent=peref_menu;
+	peref_menu->addRow({"Платы расширения",15})->children.push_back(rashirenie_peref);
+		rashirenie_peref->parent=peref_menu;
+		rashirenie_peref->addRow({"bak",43})->children.push_back(bak_peref);
+			bak_peref->addRow({"status",44})->parent=rashirenie_peref;
 
 	//������������������������������������������������������ ������������������������������������������������������������
-	setting_menu->addRow({"kalibrovka",16})->parent=start_menu;
-	setting_menu->addRow({"ustirovka",17})->parent=start_menu;
-	setting_menu->addRow({"price",18})->parent=start_menu;
-	setting_menu->addRow({"nichnie",19})->parent=start_menu;
-	setting_menu->addRow({"plat rashire",20})->parent=start_menu;
-
+	setting_menu->parent=start_menu;
+	setting_menu->addRow({"kalibrovka",16})->children.push_back(kalibrovka);
+		kalibrovka->addRow({"rashod",45})->parent=setting_menu;
+	setting_menu->addRow({"ustirovka",17})->children.push_back(ustirowka);
+		ustirowka->addRow({"obem 1",46})->parent=setting_menu;
+		ustirowka->addRow({"obem 2",47})->parent=setting_menu;
+		ustirowka->addRow({"obem 3",48})->parent=setting_menu;
+	setting_menu->addRow({"price",18})->children.push_back(price);
+		price->parent=setting_menu;
+		price->addRow({"obem",49})->children.push_back(price_obem);
+			price_obem->addRow({"obem 1",51})->parent=price;
+			price_obem->addRow({"obem 2",52})->parent=price;
+			price_obem->addRow({"obem 3",53})->parent=price;
+		price->addRow({"cena",50})->children.push_back(price_cena);
+			price_cena->addRow({"obem 1",54})->parent=price;
+			price_cena->addRow({"obem 2",55})->parent=price;
+			price_cena->addRow({"obem 3",56})->parent=price;
+	setting_menu->addRow({"nichnie",19})->children.push_back(setting_nal);
+		setting_nal->parent=setting_menu;
+		setting_nal->addRow({"kupur",57})->children.push_back(setting_kupur);
+			setting_kupur->addRow({"inkas",58})->parent=nal_stat;
+	setting_menu->addRow({"plat rashire",20})->children.push_back(setting_rashir);
+		setting_rashir->parent=setting_menu;
+		setting_rashir->addRow({"bak",59})->children.push_back(bak_setting);
+			bak_setting->addRow({"rezhim",60})->parent=setting_rashir;
+		setting_rashir->addRow({"zamena",61})->children.push_back(zamena);
+			zamena->parent=setting_rashir;
+			zamena->addRow({"data",62})->children.push_back(data_zapr);
+				data_zapr->addRow({"manual",63})->parent=zamena;
+				data_zapr->addRow({"auto",64})->parent=zamena;
+			zamena->addRow({"data",65})->children.push_back(data_zamen);
+				data_zamen->addRow({"manual",66})->parent=zamena;
+				data_zamen->addRow({"auto",67})->parent=zamena;
 
 }
 
@@ -86,91 +218,34 @@ void menu_output(Row* row)
 	for(int i=0;i<8;i+=1) //������������������������������������������������������������ ������������������������������ ������������ ������������������������������������������
 	{
 		sprintf(ch, "%s", row->payload.name.c_str());
-		int test[50];
-
-		switch (row->payload.name_int) {
-			case 1:
-				glcd_int_puts(str1, 10, i);
-				break;
-			case 2:
-				glcd_int_puts(str2, 10, i);
-							break;
-			case 3:
-				glcd_int_puts(str3, 10, i);
-				break;
-			case 4:
-							glcd_int_puts(str4, 10, i);
-							break;
-			case 5:
-							glcd_int_puts(str5, 10, i);
-							break;
-			case 6:
-							glcd_int_puts(str6, 10, i);
-							break;
-			case 7:
-							glcd_int_puts(str7, 10, i);
-							break;
-			case 8:
-							glcd_int_puts(str8, 10, i);
-							break;
-			case 9:
-							glcd_int_puts(str9, 10, i);
-							break;
-			case 10:
-							glcd_int_puts(str10, 10, i);
-							break;
-			case 11:
-							glcd_int_puts(str11, 10, i);
-							break;
-			case 12:
-							glcd_int_puts(str12, 10, i);
-							break;
-			case 13:
-							glcd_int_puts(str13, 10, i);
-							break;
-			case 14:
-							glcd_int_puts(str14, 10, i);
-							break;
-			case 15:
-							glcd_int_puts(str15, 10, i);
-							break;
-			case 16:
-							glcd_int_puts(str16, 10, i);
-							break;
-			case 17:
-							glcd_int_puts(str17, 10, i);
-							break;
-			case 18:
-							glcd_int_puts(str18, 10, i);
-							break;
-			case 19:
-							glcd_int_puts(str19, 10, i);
-							break;
-			case 20:
-							glcd_int_puts(str20, 10, i);
-
-							break;
+		int a=row->payload.name_int;
+		int test[14];
+		for(int i=0;i<14;i++)
+		{
+			test[i]=str[row->payload.name_int-1][i];
 		}
+		glcd_int_puts(test, 10, i);
 
 
 
-//		if(row->payload.value != NULL)
-//		{
-//			if(row->payload.callback == input_value_date_ptr || row->payload.callback == input_value_time_ptr)
-//			{
-//				sprintf(ch,"%02d.%02d.%02d",(uint8_t)(*row->payload.value>>16),(uint8_t)(*row->payload.value>>8),(uint8_t)(*row->payload.value));
-//
-//			}
-//			else if(row->payload.callback == input_value_range10_ptr)
-//			{
-//				sprintf(ch,"%02d",*row->payload.value);
-//			}
-//			else  if(row->payload.selection == NULL)
-//			{
-//				sprintf(ch,"%03d",*row->payload.value);
-//			}
-//			glcd_puts(ch, row->payload.lcd_data_position, i);
-//		}
+
+		//		if(row->payload.value != NULL)
+		//		{
+		//			if(row->payload.callback == input_value_date_ptr || row->payload.callback == input_value_time_ptr)
+		//			{
+		//				sprintf(ch,"%02d.%02d.%02d",(uint8_t)(*row->payload.value>>16),(uint8_t)(*row->payload.value>>8),(uint8_t)(*row->payload.value));
+		//
+		//			}
+		//			else if(row->payload.callback == input_value_range10_ptr)
+		//			{
+		//				sprintf(ch,"%02d",*row->payload.value);
+		//			}
+		//			else  if(row->payload.selection == NULL)
+		//			{
+		//				sprintf(ch,"%03d",*row->payload.value);
+		//			}
+		//			glcd_puts(ch, row->payload.lcd_data_position, i);
+		//		}
 		if(row->payload.selection != NULL)
 		{
 			string str = row->payload.selection->text[row->payload.selection->TypeNow];
